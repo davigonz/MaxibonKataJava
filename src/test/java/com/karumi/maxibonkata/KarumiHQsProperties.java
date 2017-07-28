07679356e42e52a24a0f525e03380cb55e80c867
davigonz <dgonzalez@solidgear.es>
2017-07-28 14:18:59 +0200

    Adding more tests

---
+++ src/test/java/com/karumi/maxibonkata/KarumiHQsProperties.java
@@ -0,0 +1,32 @@
+package com.karumi.maxibonkata;
+
+import com.pholser.junit.quickcheck.From;
+import com.pholser.junit.quickcheck.Property;
+import com.pholser.junit.quickcheck.runner.JUnitQuickcheck;
+
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by davidgonzalez on 28/07/17.
+ */
+
+@RunWith(JUnitQuickcheck.class) public class KarumiHQsProperties {
+
+    private static final String ANY_NAME = "Pedro";
+    private static final int ANY_NUMBER_OF_MAXIBONS = 10;
+
+    @Property (trials = 200)
+    public void theMaxibonsLeftCannotBeLowerOrEqualThanTwo (@From(DevelopersGenerator.class)
+                                                                        Developer developer) {
+
+        KarumiHQs karumiHQs = new KarumiHQs();
+
+        karumiHQs.openFridge(developer);
+
+        assertTrue(karumiHQs.getMaxibonsLeft() > 2);
+    }
+}
